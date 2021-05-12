2066217344920bab68cd32d0370bd1a76edd186e
Kevin Botero <kbotero201@gmail.com>
2021-05-12 15:59:55 -0400

    push + pop + peek + delete methods

--- stack.py
+++ stack.py
@@ -8,3 +8,32 @@ class Stack:
     def __init__(self):
         self.head = None 
         self.tail = None
+
+    def push(self, value):
+        newNode = Node(value)
+        if self.head == None:
+            self.head = newNode
+            self.tail = newNode
+        else: 
+            newNode.next = self.head 
+            self.head = newNode
+
+    def pop(self):
+        if self.head == None:
+            return None 
+        remNode = self.head 
+        if self.head == self.tail:
+            self.head = None 
+            self.tail = None 
+        else: 
+            self.head = self.head.next 
+        return remNode 
+
+    def peek(self):
+        if self.head == None 
+            return None
+        else: 
+            return self.head
+
+    def delete(self):
+        self.head = None 

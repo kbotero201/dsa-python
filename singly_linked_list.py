3366998a1f62d736a48e2f20007f76585cdd3be6
Kevin Botero <kbotero201@gmail.com>
2021-05-14 11:36:13 -0400

    update

---
+++ singly_linked_list.py
@@ -0,0 +1,48 @@
+
+class Node: 
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None 
+        self.tail = None
+    
+    #O(n) time complexity & O(1) space complexity 
+    def traverse(self):
+        if self.head == None:
+            return None 
+        else: 
+            current = self.head
+            while current is not None:
+                print(current.value)
+                current = current.next 
+    
+    #O(n) time complexity & O(1) space complexity 
+    def search(self, value):
+        if self.head == None:
+            return None
+        current = self.head
+        while current is not None
+            if value == current.value:
+                return current
+            else: 
+                current = current.next 
+        return None
+
+    #O(n) time complexity & O(1) space complexity 
+    def getIndex(self, index):
+        if self.head == None:
+            return None
+        current = self.head
+        counter = 0
+        while current is not None:
+            if index == counter:
+                return current
+            else: 
+                current = current.next 
+                counter += 1
+        return None 
+            
+    

bf5fbe7437363d44290d594e40783e086395df0e
Kevin Botero <kbotero201@gmail.com>
2021-05-17 17:41:10 -0400

    insert function

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -59,5 +59,32 @@ class SLinkedList:
                 current = current.next 
                 counter += 1
         return False 
-            
-    
+
+
+    #O(n) time complexity & O(1) space complexity 
+    def insert(self, index, value):
+        newNode = Node(value)
+        if self.head == None: 
+            self.head = newNode
+            self.tail = newNode
+        else: 
+            if index == 0:
+                newNode.next = self.head
+                self.head = newNode
+            elif index == -1:
+                self.tail.next = newNode
+                self.tail = newNode
+            else: 
+                current = self.head
+                nextNode = current.next
+                counter = 0
+                while current is not None:
+                    if index == counter -1:
+                        current.next = newNode 
+                        newNode.next = nextNode
+                        return "Inserted"
+                    else: 
+                        current = current.next 
+                        counter += 1
+                return "Index not found"
+

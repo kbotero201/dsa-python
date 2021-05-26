6058c5ccae29e7f52bc566ed3d164de882e5a119
Kevin Botero <kbotero201@gmail.com>
2021-05-26 14:02:50 -0400

    Inserting + deleting

--- binary_tree.py
+++ binary_tree.py
@@ -80,6 +80,63 @@ def BFS(rootNode):
                 queue.enqueue(root.data.left)
             if root.data.right is not None:
                 queue.enqueue(root.data.right)
-        
 
 
+
+
+# Searching for a node in Binary Tree -----------
+# We will use level order traversal for better performance:
+# Note: Queue always perfroms better than stacks/recursion  
+
+# O(n) time complexity & O(n) space complexity 
+def searchBT(rootNode, value):
+    if not rootNode:
+        return 
+    else: 
+        queue = Queue()
+        queue.enqueue(rootNode)
+        while not (queue.isEmpty()):
+            root = queue.dequeue()
+            if root.data.value == value:
+                return "Sucess"
+            if root.data.left is not None:
+                queue.enqueue(root.data.left)
+            if root.data.right is not None:
+                queue.enqueue(root.data.right)
+        return "Value Not Found"
+
+
+# Inserting node in Binary Tree -----------
+# O(n) time complexity & O(n) space complexity 
+def insert(rootNode, newNode):
+    if not rootNode: 
+        rootNode = newNode 
+    else:
+        queue = Queue()
+        queue.enqueue(rootNode)
+        while not (queue.isEmpty()):
+            root = queue.dequeue()
+            if root.data.left is not None:
+                queue.enqueue(root.data.left)
+            else: 
+                root.data.left = newNode
+                return "Inserted"
+            if root.data.right is not None:
+                queue.enqueue(root.data.right)
+            else:
+                root.data.right = newNode
+                return "Inserted"
+
+
+# Deleting a node in Binary Tree -----------
+
+
+
+
+# Deleting Entire Binary Tree -----------
+# O(1) time complexity & O(1) space complexity 
+def deleteBT(rootNode):
+    rootNode.value = None 
+    rootNode.left = None
+    rootNode.right = None
+    return "The Binary Tree Has Been Deleted"

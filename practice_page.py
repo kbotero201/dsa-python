98fb04869fab60e574384fdba3330f746a04832a
Kevin Botero <kbotero201@gmail.com>
2021-07-15 19:20:49 -0400

    sorting

--- practice_page.py
+++ practice_page.py
@@ -347,3 +347,11 @@ def search(rootNode, val):
                     return "Found"
                 current = current.right
         return "Not Found"
+
+#Sorting 
+def bubbleSort(list):
+    for i in range(len(list) -1):
+        for j in range(len(list) - i -1):
+            if list[j] > list[j+1]:
+                list[j], list[j+1] = list[j+1], list[j]
+    print(list)

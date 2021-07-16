df933b8eb1a7c9b1ec44aea5c30e7f7ec0066900
Kevin Botero <kbotero201@gmail.com>
2021-07-16 09:14:27 -0400

    Selection sort

--- practice_page.py
+++ practice_page.py
@@ -355,3 +355,12 @@ def bubbleSort(list):
             if list[j] > list[j+1]:
                 list[j], list[j+1] = list[j+1], list[j]
     print(list)
+
+def selectionSort(list):
+    for i in range(len(list)):
+        min_index = i
+        for j in range(i+1, len(list)):
+            if list[min_index] > list[j]:
+                min_index = j
+        list[i], list[min_index] = list[min_index], list[i]
+    print(list)

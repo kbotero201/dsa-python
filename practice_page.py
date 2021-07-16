d03d5d23f674be9bd1281627c10acd0106eb830d
Kevin Botero <kbotero201@gmail.com>
2021-07-16 09:14:48 -0400

    Insertion sort

--- practice_page.py
+++ practice_page.py
@@ -364,3 +364,13 @@ def selectionSort(list):
                 min_index = j
         list[i], list[min_index] = list[min_index], list[i]
     print(list)
+
+def insertionSort(list):
+    for i in range(1, len(list)):
+        key = list[i]
+        j = i-1
+        while j >= 0 and key < list[j]:
+            list[j+1] = list[j]
+            j -= 1
+        list[j + 1] = key 
+    print(list)

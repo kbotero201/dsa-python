b37e9282562d5d4fe4436779a5a1c1b63aacf190
Kevin Botero <kbotero201@gmail.com>
2021-07-21 23:00:01 -0400

    Merge Sort Iterative

--- practice_page.py
+++ practice_page.py
@@ -404,3 +404,18 @@ def mergeSort(array):
 
     left, right = mergeSort(array[:midpoint]), mergeSort(array[midpoint:])
     return merge(left, right)
+
+def merge(left, right):
+    result = []
+    left_pointer = right_pointer = 0
+
+    while left_pointer < len(left) and right_pointer < len(right):
+        if left[left_pointer] < right[right_pointer]:
+            result.append(left[left_pointer])
+            left_pointer += 1
+        else:
+            result.append(right[right_pointer])
+            right_pointer += 1
+    result.extend(left[left_pointer:])
+    result.extend(right[right_pointer:])
+    return result 
